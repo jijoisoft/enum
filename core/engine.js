871c162270cc3b72dc4763b3e55722b9fc70320b
Jijo Joseph <48672501+jijoisoft@users.noreply.github.com>
2019-03-18 18:17:12 +0530

    Add files via upload

--- core/engine.js
+++ core/engine.js
@@ -1,1 +1,19 @@
-
+module.exports.process = function process(qry, callback){
+    
+}
+module.exports.getPhoneFromQuery = function getPhoneFromQuery(qry) {
+    var phoneRev = qry.split("@");
+    phoneRev = phoneRev[0].replace(/[^0-9]/g, '');
+    var phone = "";
+    for (var i = phoneRev.length - 1; i >= 0; i--) {
+        phone += phoneRev[i];
+    }
+    return phone;
+}
+module.exports.getResponseForPhone = function getResponseForPhone(phone) { // add arg , callback
+    var response = 1;
+    return response;
+}
+module.exports.getResponseForPhoneFromDB = function getResponseForPhoneFromDB(){
+    ;
+}
